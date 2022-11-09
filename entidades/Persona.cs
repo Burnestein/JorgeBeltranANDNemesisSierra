056c0fab8db037ce5ceb472edeaa6bcce42725ab
desjorunmx <jordybl75@gmail.com>
2022-11-08 18:14:56 -0600

    Entidades
    Crear Persona y Usuario

---
+++ entidades/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JorgeBeltranANDNemesisSierra.entidades
+{
+    class Persona
+    {
+        private String nombre;
+        private String apepat;
+        private String apemat;
+        private String calle;
+        private int numero;
+        private String colonia;
+        private String estado;
+        private String numcelular;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apepat { get => apepat; set => apepat = value; }
+        public string Apemat { get => apemat; set => apemat = value; }
+        public string Calle { get => calle; set => calle = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public string Colonia { get => colonia; set => colonia = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public string Numcelular { get => numcelular; set => numcelular = value; }
+    }
+}
